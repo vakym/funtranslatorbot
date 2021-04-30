2ddfa0e87ddffecc50e64381260ea50d555b3a64
Евгений Аввакумов <avakym92@gmail.com>
2021-04-30 20:56:20 +0300

    test commit

---
+++ funtranslatorbot/CalculatorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace funtranslatorbot
+{
+    public class CalculatorService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
