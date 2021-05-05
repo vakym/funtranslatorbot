e4d5eb67a3940f00f2bde563a6057ae0e16edb02
sova.krs@gmail.com <sova.krs@gmail.com>
2021-05-05 20:08:38 +0300

    реализован класс Translator, методы GetTranslation, GetTextMessageFromJson

--- funtranslatorbot/translationAPI/Translator.cs
+++ funtranslatorbot/translationAPI/Translator.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
 
 namespace funtranslatorbot.translationAPI
 {
     public class Translator : ITranslator
     {
-        private readonly string baseHostURL = "https://translate.yandex.net/api/v1.5/tr.json";
-        private readonly string APIKey = "";
+        private readonly string baseHostURL = "https://api.cognitive.microsofttranslator.com";
+        private readonly string APIKey = "0955c66aaea5418595388eb967bc2b2a";
         private readonly string textFormat = "plain";
         private readonly HttpClient httpClient = new HttpClient();
         public string GetTextLanguage(string inputText)
@@ -20,7 +22,28 @@ namespace funtranslatorbot.translationAPI
 
         public string GetTranslation(string inputText, string sourceLanguage, string targetLanguage)
         {
-            throw new NotImplementedException();
+            var url = baseHostURL.AppendPathSegment("/translate")
+                                 .SetQueryParam("api-version", "3.0")
+                                 .SetQueryParams(new
+                                 {
+                                     from = sourceLanguage,
+                                     to = targetLanguage
+
+                                 }).ToString();
+            var result = url.WithHeader("Ocp-Apim-Subscription-Key", APIKey)
+                            .WithHeader("Ocp-Apim-Subscription-Region", "westeurope")
+                            .PostJsonAsync(new[] { new { text = inputText } })
+                            .Result;
+            var stringResponse = result.ResponseMessage.Content.ReadAsStringAsync().Result;
+            return GetTextMessageFromJson(stringResponse);
+                        
+        }
+        private string GetTextMessageFromJson (string inputJson)
+        {
+            int trimBeginCount = 27;
+            int trimEndCount = 15;
+            return inputJson.Substring(trimBeginCount, inputJson.Length - trimBeginCount - trimEndCount);
         }
     }
+    
 }
