b46f17f4d39df6e7ce776d8f697ec07fbb31671f
sova.krs@gmail.com <sova.krs@gmail.com>
2021-05-05 18:06:05 +0300

    созданы интерфейсы, добавлен класс Translator

---
+++ funtranslatorbot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using funtranslatorbot.translationAPI;
+
+namespace funtranslatorbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           
+            
+        }
+    }
+}
---
+++ funtranslatorbot/translationAPI/ITranslator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funtranslatorbot.translationAPI
+{
+    public interface ITranslator
+    {
+        string GetTranslation(string inputText, string sourceLanguage, string targetLanguage);
+        string GetTextLanguage(string inputText);
+    }
+}
