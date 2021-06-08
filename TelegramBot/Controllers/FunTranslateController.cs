9604e0ff655434f5e5ab7a00ddcc1cc1ba6c234f
sova.krs@gmail.com <sova.krs@gmail.com>
2021-06-08 18:50:43 +0300

    создала контроллер, подключила библиотеку

---
+++ TelegramBot/Controllers/FunTranslateController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TranslationLib;
+
+namespace FunTranslateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FunTranslateController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var translateRURU = new TranslateRURU();
+            return translateRURU.TranslateCount("сорок обезьян чо то там банан", 5);
+        }
+    }
+}
